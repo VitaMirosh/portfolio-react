44fecb90f9afea7f2adc09039c1c4ce1623eb189
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2023-10-02 13:23:25 +0300

    add my portfolio button and context

--- src/contacts/Contact.tsx
+++ src/contacts/Contact.tsx
@@ -8,11 +8,11 @@ export function Contact() {
             <div className={`${f.container} ${s.contacts}`}>
                 <h3> Contacts </h3>
                 <form className={s.formStyle}>
-                    <div className={s.input1}><input/></div>
-                    <div className={s.input2}><input/></div>
-                    <textarea/>
+                    <div className={s.input}><input/></div>
+                    <div className={s.input}><input/></div>
+                    <div className={s.textarea}><textarea/></div>
+                    <button className={s.buttonContact}>Send</button>
                 </form>
-                <button className={s.buttonContact}>Send</button>
             </div>
         </div>
 
